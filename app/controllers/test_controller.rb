ced62da82c56772ea5b77638a71030d36dd77d80
ABOSAUDTECH <abosaudtech@gmail.com>
2021-04-12 17:47:15 +0300

    Lesson 12

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,12 @@
 class TestController < ApplicationController
   layout false
+
+  
   def first
+    render(:tamplate => 'test/first') # We can write this line in another way in second def
+  end
+  
+  def second
+    render('second')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'test#first'
   get 'test/first'
+  get 'test/second'
   #match 'test/first', :to => 'test#first', :via => :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
