aa0dacd09c0c359e7877da1386366d58bbbe0439
ABOSAUDTECH <abosaudtech@gmail.com>
2021-04-12 11:58:26 +0300

    Routes Lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'test#first'
   get 'test/first'
+  #match 'test/first', :to => 'test#first', :via => :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
