b9179364fb89797ca5376b452ea89cdf1edf1309
Abo Saud <38051894+abosaudtech@users.noreply.github.com>
2021-04-17 15:59:37 +0300

    Update Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'test#first'
   get 'test/first'
   get 'test/second'
-  #match 'test/first', :to => 'test#first', :via => :get
+  # match 'test/first', :to => 'test#first', :via => :get
+  # match 'test/second', :to => 'test#second', :via => :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
