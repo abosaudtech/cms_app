088c050cb8fb4b6d3cc50f81ac5db4304fdb24bc
ABOSAUDTECH <abosaudtech@gmail.com>
2021-04-25 10:28:28 +0300

    create_column

--- db/migrate/20210422105824_create_admins.rb
+++ db/migrate/20210422105824_create_admins.rb
@@ -1,8 +1,22 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def up
     create_table :admins do |t|
+      t.column "first_name" :string, :limit => 25
+      t.string "last_name" :limit => 50
+      t.string "email" :default => value " ", :null = false 
+      t.string "password", :limit => 100
+      t.datetime "created_at"
+      t.datetime "updated_at" 
 
       t.timestamps
+
+      # binary - boolean - date - datetime - float - integer - string - text - time
+      # :limit => size
+      # :defult => value 
+      # :null => true/false
+      # :precisioin => number
+      # :scale => number
+
     end
   end
 
