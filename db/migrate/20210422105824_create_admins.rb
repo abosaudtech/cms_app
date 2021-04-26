942f93ab71f88c1551da40c06bfb81eddebe4346
ABOSAUDTECH <abosaudtech@gmail.com>
2021-04-26 15:00:40 +0300

    migration 2

--- db/migrate/20210422105824_create_admins.rb
+++ db/migrate/20210422105824_create_admins.rb
@@ -1,13 +1,14 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def up
     create_table :admins do |t|
-      t.column "first_name" :string, :limit => 25
-      t.string "last_name" :limit => 50
-      t.string "email" :default => value " ", :null = false 
+      t.column "first_name", :string, :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => '', :null => false 
       t.string "password", :limit => 100
-      t.datetime "created_at"
-      t.datetime "updated_at" 
-
+      
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+      # we can dispense with the tow lines above with the line below:
       t.timestamps
 
       # binary - boolean - date - datetime - float - integer - string - text - time
